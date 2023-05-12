016241024aa0aa16733a032a8dd52aa9f6697bd2
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2023-05-11 21:55:30 -0300

    Adding the license text to the source files

--- pyspis/attitude.py
+++ pyspis/attitude.py
@@ -1,3 +1,26 @@
+#
+# attiude.py
+# 
+# Copyright The PySPIS Contributors.
+# 
+# This file is part of PySPIS library.
+# 
+# PySPIS library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PySPIS library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with PySPIS library. If not, see <http://www.gnu.org/licenses/>.
+# 
+#
+
+
 import numpy as np
 
 def attitude(h, e, RA, i, w, TA,it,Q,T,r_sun,t):
--- pyspis/j0.py
+++ pyspis/j0.py
@@ -1,3 +1,26 @@
+#
+# j0.py
+# 
+# Copyright The PySPIS Contributors.
+# 
+# This file is part of PySPIS library.
+# 
+# PySPIS library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PySPIS library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with PySPIS library. If not, see <http://www.gnu.org/licenses/>.
+# 
+#
+
+
 def J0(year, month, day):
     jd = 367 * year - int(7 * (year + int((month + 9) / 12)) / 4) + int(275 * month / 9) + day + 1721013.5
     return jd
